aa154ac68f903c1f049fbc2611cf302e9cf02d19
quandangv <quandangv@gmail.com>
2020-12-09 19:50:01 +0700

    Build: Fix build script

--- archive.sh
+++ archive.sh
@@ -1,9 +1,11 @@
+proj_name=cspace
+
 [[ -z "$1" ]] && {
   echo "Please pass version as argument"
   exit 1
 }
 
-path=./build/cspace-$1
+path=./build/$proj_name-$1
 git archive --prefix cspace/ --output $path.tar --format tar HEAD &&
 git archive --prefix cspace/ --output $path.tar.gz --format tar.gz HEAD && {
   echo "Archive generated to $path"
