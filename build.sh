29b49b301a387dc70b1cb035a7fbaf82e99ac15a
quandangv <quandangv@gmail.com>
2021-01-25 16:19:23 +0700

    Update cmake_module

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash 
 PROJECT=$(grep -oP 'project\(\K[\w]+' CMakeLists.txt)
-source cmake/build.sh
 
-parse $@
-ask
-main
+#git submodule update --init --recursive -- cmake
+
+source cmake/utils.sh
+parse_options $@
+ask_options
+build
 
