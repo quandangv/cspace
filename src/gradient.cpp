4f3de71293db7fa97f229c3c02545bf26c8bdeb8
quandangv <quandangv@gmail.com>
2021-04-08 15:14:29 +0700

    Fix CMake syntax errors

--- src/gradient.cpp
+++ src/gradient.cpp
@@ -71,7 +71,7 @@ std::string gradient<dimension>::get_hex(double at) const {
   double tmp[dimension];
   get(at, tmp);
   std::stringstream ss;
-  ss << std::ios::hex << '#';
+  ss << std::hex << '#';
   for(int i = 0; i < dimension; i++)
     ss << std::setw(2) << lround(tmp[i]*255);
   return ss.str();
@@ -79,21 +79,27 @@ std::string gradient<dimension>::get_hex(double at) const {
 
 template<int dimension>
 void gradient<dimension>::add_hex(double at, const std::string& str, bool alpha_first) {
-  if constexpr(dimension != 4)
-    throw std::logic_error("Incompatible dimensions");
   bool alpha;
-  auto data = points.emplace(at, std::array<double, dimension>()).first->second.data();
-  if (str.empty() || str.front() != '#' || !parse_hex(tstring(str.data() + 1), data, alpha))
-    throw std::logic_error("Invalid hex string: " + str);
-  if (!alpha) {
-    if (alpha_first) {
-      data[3] = data[2];
-      data[2] = data[1];
-      data[1] = data[0];
-      data[0] = 1;
-    } else
-      data[3] = 1;
-  }
+  if constexpr(dimension == 4) {
+    auto data = points.emplace(at, std::array<double, dimension>()).first->second.data();
+    if (str.empty() || str.front() != '#' || !parse_hex(tstring(str.data() + 1), data, alpha))
+      throw std::logic_error("Invalid hex string: " + str);
+    if (!alpha) {
+      if (alpha_first) {
+        data[3] = data[2];
+        data[2] = data[1];
+        data[1] = data[0];
+        data[0] = 1;
+      } else
+        data[3] = 1;
+    }
+  } else if constexpr(dimension == 3) {
+    std::array<double, 4> tmp;
+    if (str.empty() || str.front() != '#' || !parse_hex(tstring(str.data() + 1), tmp.data(), alpha) || alpha)
+      throw std::logic_error("Invalid hex string: " + str);
+    points.emplace(at, std::array<double, 3>{tmp[0], tmp[1], tmp[2]});
+  } else
+    throw std::logic_error("Incompatible dimensions");
 }
 
 }
